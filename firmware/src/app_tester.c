7e4b862a8351aa1ab350519b2ec4be102290d76b
iPAS <ptiwatthanont@gmail.com>
2022-09-13 21:44:35 +0700

    DO_TEST --> RUN_TESTER

--- firmware/src/app_tester.c
+++ firmware/src/app_tester.c
@@ -147,7 +147,7 @@ void APP_TESTER_Tasks ( void )
             {
                 app_testerData.state = APP_TESTER_STATE_SERVICE_TASKS;
 
-                #if defined(DO_TEST) && (DO_TEST == 0)
+                #if defined(RUN_TESTER) && (RUN_TESTER == 0)
                 // app_testerData.state = APP_TESTER_STATE_FINISH;
                 vTaskSuspend(NULL);  // Suspend itself
                 #endif
